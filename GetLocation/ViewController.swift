dfda1357d3bec1250507c89869397b9f890d446a
xiechengxi <243653517@qq.com>
2018-11-05 16:35:11 +0800

    Initial Commit

---
+++ GetLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetLocation
+//
+//  Created by Apple on 2018/11/5.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
