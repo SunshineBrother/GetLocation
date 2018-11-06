1331df8a02b37e55c514821990a4062f9414ff09
xiechengxi <243653517@qq.com>
2018-11-06 09:38:23 +0800

    gitlocation

--- GetLocation/ViewController.swift
+++ GetLocation/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var areaLabel: UILabel!
+    @IBOutlet weak var localLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
-}
+    @IBAction func getMessage(_ sender: Any) {
+        
+        LocationUtil.share.getCurrentPointLocation(isOnce: false) { (loc, errorMsg) in
+            if errorMsg == nil {
+                self.localLabel.text = String(format: "%f,%f", (loc?.coordinate.latitude)!,(loc?.coordinate.longitude)!)
+            }
+        }
+        
+        
 
+    }
+
+    @IBAction func getMessage2(_ sender: Any) {
+        LocationUtil.share.getCurrentLocation(isOnce: false) { (loc, errorMsg) -> () in
+            if errorMsg == nil {
+                self.areaLabel.text = String(format: "%@", (loc?.name)!)
+            }
+            
+        }
+    }
+}
